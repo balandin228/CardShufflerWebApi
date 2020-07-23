d93b72a87e572078d54175b7845c56379b7d5352
Balandin228 <igorek.chelovek@gmail.com>
2020-07-23 15:37:04 +0500

    add interface IDecBuilder

---
+++ TestApi.CardShuffler/DeckBuilder/IDeckBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestApi.Core.Domain.Card;
+
+namespace TestApi.Core.DeckBuilder
+{
+    public interface IDeckBuilder
+    {
+        IEnumerable<Card> Cards { get; }
+        IEnumerable<Card> Shuffle();
+        IEnumerable<Card> ChangeDeck(IEnumerable<Card> cards);
+    }
+}

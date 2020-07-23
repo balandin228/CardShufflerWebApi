18ec542879b0c4c887427761d04d74d17ed1945f
Balandin228 <igorek.chelovek@gmail.com>
2020-07-23 15:49:43 +0500

    Create Test Project

---
+++ TestApi.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApi.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
